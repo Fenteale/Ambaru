2fdb9a55c5b6cfd019f1b39eb8eed015e72d6797
mangmasta <mangmasta@gmail.com>
2017-09-08 14:09:48 -0700

    Initial Commit

---
+++ Assets/Scripts/CameraStable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStable : MonoBehaviour {
+
+    public GameObject car;
+    public float carx, cary, carz;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        carx = car.transform.eulerAngles.x;
+        cary = car.transform.eulerAngles.y;
+        carz = car.transform.eulerAngles.z;
+
+        transform.eulerAngles = new Vector3(0, cary, 0);
+	}
+}
