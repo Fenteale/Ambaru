e9cd0d6111ccfbba800f46e734bbbd2ef92908c2
Mangmasta <mangmasta@aim.com>
2017-10-08 05:29:46 -0700

    Added model rotation and wheels on the ground
    Nothing is working quite right, but its a first step into a better design...

--- Assets/AmbaruCharacter.cs
+++ Assets/AmbaruCharacter.cs
@@ -9,12 +9,36 @@ public class AmbaruCharacter : MonoBehaviour
     public float turnSpeed;
     public float gravity = 1.0f;
 
-    private Rigidbody rb;
+    public GameObject FrontWheel;
+    public GameObject BackWheel;
+
+    private GameObject fwpos;
+    private GameObject bwpos;
+    private float bwrad;
+    private float wheeldistance;
     private bool eGravity;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+
+        fwpos = this.gameObject.transform.GetChild(1).gameObject;
+        bwpos = this.gameObject.transform.GetChild(2).gameObject;
+
+        //Physics.IgnoreCollision(FrontWheel.GetComponent<Collider>(), GetComponent<Collider>());
+        //Physics.IgnoreCollision(BackWheel.GetComponent<Collider>(), GetComponent<Collider>());
+
+        FrontWheel.transform.SetPositionAndRotation(new Vector3(fwpos.gameObject.transform.position.x, fwpos.transform.position.y, fwpos.gameObject.transform.position.z), Quaternion.Euler(fwpos.transform.rotation.eulerAngles.x, FrontWheel.transform.rotation.eulerAngles.y, FrontWheel.transform.rotation.eulerAngles.z));
+        BackWheel.transform.SetPositionAndRotation(new Vector3(bwpos.gameObject.transform.position.x, bwpos.transform.position.y, bwpos.gameObject.transform.position.z), Quaternion.Euler(bwpos.transform.rotation.eulerAngles.x, BackWheel.transform.rotation.eulerAngles.y, BackWheel.transform.rotation.eulerAngles.z));
+        bwrad = BackWheel.GetComponent<CapsuleCollider>().radius;
+        wheeldistance = fwpos.transform.localPosition.z;
+    }
+
+    private void LateUpdate()
+    {
+        FrontWheel.transform.localPosition = new Vector3(fwpos.transform.localPosition.x, FrontWheel.transform.localPosition.y, fwpos.transform.localPosition.z);
+        
+        //FrontWheel.transform.SetPositionAndRotation(new Vector3(fwpos.gameObject.transform.position.x, FrontWheel.transform.position.y, fwpos.gameObject.transform.position.z), Quaternion.Euler(fwpos.transform.rotation.eulerAngles.x, FrontWheel.transform.rotation.eulerAngles.y, FrontWheel.transform.rotation.eulerAngles.z));
+        //BackWheel.transform.SetPositionAndRotation(new Vector3(bwpos.gameObject.transform.position.x, BackWheel.transform.position.y, bwpos.gameObject.transform.position.z), Quaternion.Euler(bwpos.transform.rotation.eulerAngles.x, BackWheel.transform.rotation.eulerAngles.y, BackWheel.transform.rotation.eulerAngles.z));
     }
 
     void FixedUpdate()
@@ -22,33 +46,40 @@ public class AmbaruCharacter : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 oldpos;
-
+        //MOVEMENT CODE
         Vector3 movement = new Vector3(0.0f, 0.0f, moveVertical);
+        transform.Rotate(0, moveHorizontal * turnSpeed * moveVertical, 0);
+        //END MOVEMENT CODE
 
-        oldpos = transform.position;
-        //rb.transform.eulerAngles = new Vector3(Mathf.Clamp(rb.rotation.x, 0.0f, 0.0f), rb.rotation.y, rb.rotation.z);
-	transform.Rotate(0, moveHorizontal * turnSpeed * moveVertical, 0);
-	
-        //transform.eulerAngles = new Vector3(Mathf.Clamp(transform.eulerAngles.x, 0.0f, 0.0f), transform.eulerAngles.y, transform.eulerAngles.z);
+        //SET WHEELS ON GROUND
+        RaycastHit hit;
+        Ray downRay = new Ray(transform.position + Vector3.up, -Vector3.up);
+        if(Physics.Raycast(downRay, out hit))
+        {
+            BackWheel.transform.localPosition = new Vector3(BackWheel.transform.localPosition.x, -hit.distance+bwrad+1f, BackWheel.transform.localPosition.z);
+        }
+
+        downRay = new Ray(fwpos.transform.position+Vector3.up, -Vector3.up);
+        if (Physics.Raycast(downRay, out hit))
+        {
+            FrontWheel.transform.localPosition = new Vector3(FrontWheel.transform.localPosition.x, -hit.distance + bwrad+1f, FrontWheel.transform.localPosition.z);
+        }
+
+        //END SET WHEELS ON GROUND
+
+
+        //SET BODY ROTATION
+        float pitchAngle = Mathf.Rad2Deg * Mathf.Atan2(FrontWheel.transform.localPosition.z - BackWheel.transform.localPosition.z, FrontWheel.transform.localPosition.y - BackWheel.transform.localPosition.y);
+
+        this.gameObject.transform.GetChild(0).gameObject.transform.eulerAngles = new Vector3(180.0f-pitchAngle, transform.eulerAngles.y+180.0f, transform.eulerAngles.z);
+
+        fwpos.transform.localPosition = new Vector3(fwpos.transform.localPosition.x, fwpos.transform.localPosition.y, Mathf.Sqrt(Mathf.Pow(wheeldistance, 2) - Mathf.Pow(FrontWheel.transform.localPosition.y - BackWheel.transform.localPosition.y, 2)));
+        //END SET BODY ROTATION
+        transform.position = new Vector3(transform.position.x, BackWheel.transform.position.y, transform.position.z);
         transform.Translate(Vector3.forward * moveVertical * speed);
 
-        if (eGravity)
-        {
-            RaycastHit hit;
-            Ray downRay = new Ray(transform.position, -Vector3.up);
-            if (Physics.Raycast(downRay, out hit))
-            {
-                float hoverError = hit.distance - gravity;
-                if (hoverError > 0)
-                {
-                    transform.Translate(Vector3.down * gravity);
-                    //rb.AddForce(lift * Vector3.up);
-                }
-                else
-                    transform.Translate(Vector3.down * hit.distance);
-            }
-        }
+        
+        
 
     }
 
--- Assets/fwheel_rotation.cs
+++ Assets/fwheel_rotation.cs
@@ -6,6 +6,8 @@ public class fwheel_rotation : MonoBehaviour {
 
 
     public float rotationSpeed = 2;
+
+    public GameObject c;
 	// Use this for initialization
 	void Start () {
 		
@@ -16,7 +18,9 @@ public class fwheel_rotation : MonoBehaviour {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-
+        //transform.SetPositionAndRotation(c.transform.position, )
+        transform.position = c.transform.position;
         transform.Rotate(moveVertical*rotationSpeed, 0, 0);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, c.transform.rotation.eulerAngles.y, c.transform.rotation.eulerAngles.z);
     }
 }
