7ef7db7bf003617f0d5e76d2eb2bade9be4ea385
Mangmasta <mangmasta@aim.com>
2017-10-09 18:04:54 -0700

    Fixed more bugs

--- Assets/AmbaruCharacter.cs
+++ Assets/AmbaruCharacter.cs
@@ -15,7 +15,8 @@ public class AmbaruCharacter : MonoBehaviour
     private GameObject fwpos;
     private GameObject bwpos;
     private GameObject mod;
-    private float modoffset;
+    private float modyoffset;
+    private float modzoffset;
     private float bwrad;
     private float wheeldistance;
     private bool eGravity;
@@ -26,7 +27,8 @@ public class AmbaruCharacter : MonoBehaviour
         fwpos = this.gameObject.transform.GetChild(1).gameObject;
         bwpos = this.gameObject.transform.GetChild(2).gameObject;
         mod = this.gameObject.transform.GetChild(0).gameObject;
-        modoffset = mod.transform.localPosition.y;
+        modyoffset = mod.transform.localPosition.y;
+        modzoffset = mod.transform.localPosition.z;
 
         //Physics.IgnoreCollision(FrontWheel.GetComponent<Collider>(), GetComponent<Collider>());
         //Physics.IgnoreCollision(BackWheel.GetComponent<Collider>(), GetComponent<Collider>());
@@ -40,7 +42,7 @@ public class AmbaruCharacter : MonoBehaviour
     private void LateUpdate()
     {
         FrontWheel.transform.localPosition = new Vector3(fwpos.transform.localPosition.x, FrontWheel.transform.localPosition.y, fwpos.transform.localPosition.z);
-        mod.transform.localPosition = new Vector3(mod.transform.localPosition.x, modoffset+((FrontWheel.transform.localPosition.y - BackWheel.transform.localPosition.y) / 2.0f), mod.transform.localPosition.z);
+        mod.transform.localPosition = new Vector3(mod.transform.localPosition.x, modyoffset+((FrontWheel.transform.localPosition.y - BackWheel.transform.localPosition.y) / 2.0f), ((FrontWheel.transform.localPosition.z - BackWheel.transform.localPosition.z) / 2.0f));
         //FrontWheel.transform.SetPositionAndRotation(new Vector3(fwpos.gameObject.transform.position.x, FrontWheel.transform.position.y, fwpos.gameObject.transform.position.z), Quaternion.Euler(fwpos.transform.rotation.eulerAngles.x, FrontWheel.transform.rotation.eulerAngles.y, FrontWheel.transform.rotation.eulerAngles.z));
         //BackWheel.transform.SetPositionAndRotation(new Vector3(bwpos.gameObject.transform.position.x, BackWheel.transform.position.y, bwpos.gameObject.transform.position.z), Quaternion.Euler(bwpos.transform.rotation.eulerAngles.x, BackWheel.transform.rotation.eulerAngles.y, BackWheel.transform.rotation.eulerAngles.z));
     }
@@ -49,7 +51,7 @@ public class AmbaruCharacter : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
+        transform.Translate(Vector3.forward * moveVertical * speed);
         //MOVEMENT CODE
         Vector3 movement = new Vector3(0.0f, 0.0f, moveVertical);
         transform.Rotate(0, moveHorizontal * turnSpeed * moveVertical, 0);
@@ -81,7 +83,7 @@ public class AmbaruCharacter : MonoBehaviour
         fwpos.transform.localPosition = new Vector3(fwpos.transform.localPosition.x, fwpos.transform.localPosition.y, Mathf.Sqrt(Mathf.Pow(wheeldistance, 2) - Mathf.Pow(FrontWheel.transform.localPosition.y - BackWheel.transform.localPosition.y, 2)));
         //END SET BODY ROTATION
         //transform.position = new Vector3(transform.position.x, BackWheel.transform.position.y, transform.position.z);
-        transform.Translate(Vector3.forward * moveVertical * speed);
+        
 
         
         
