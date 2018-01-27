c5a81e0ee22417d4811e6d156194ad5353472502
Mangmasta <mangmasta@aim.com>
2018-01-26 23:06:46 -0800

    Added WheelWell and Bounce
    Made the front wheel well, and made it bounce like a spring.  still needs lots of work

--- Assets/AmbaruCharacter.cs
+++ Assets/AmbaruCharacter.cs
@@ -8,25 +8,47 @@ public class AmbaruCharacter : MonoBehaviour
 
     public float gravity = 40.0f;
 
+    public float damping = 0.0f;
+
     [Tooltip("How many units above the root of the car to start checking for collision.")]
     public float collisionOffset = 1f;
 
-    [Tooltip("The K/M Ratio used in the rotational acceleration formula (lower it is, slower the car will rotate).")]
-    public float kmRatio = 1.0f;
+    [Tooltip("The K factor used in the spring equation.")]
+    public float kFactor = 1.0f;
+
+    public float wheelMass = 1.0f;
+
+    public float bodyMass = 1.0f;
 
     public GameObject FrontWheel;
+    public GameObject FrontWheelWell;
     public GameObject BackWheel;
+
 
     public LayerMask mask = -1;
 
+
+    private Vector3 fwheelpos;
+
     private float grav = 0.0f;
+    private float omegaprime;
 
     private float rotvel = 0.0f;
+
+    private float oldtimeval1 = 0.0f;
+    private float oldtimeval2 = 0.0f;
+
+    private float fWheelXm = 0.0f;
+    private float oldhitdist = 0.0f;
 
 
     void Start()
     {
+        if (Mathf.Pow(damping / (2 * wheelMass), 2) > (kFactor / wheelMass))
+            damping = Mathf.Sqrt(kFactor / wheelMass) * 2 * wheelMass;
+        omegaprime = Mathf.Sqrt((kFactor/wheelMass)-Mathf.Pow(damping/(2*wheelMass), 2));
 
+        fwheelpos = FrontWheel.transform.localPosition;
     }
    
 
@@ -36,6 +58,7 @@ public class AmbaruCharacter : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
+        
         //END MOVEMENT CODE
 
         //SET WHEELS ON GROUND
@@ -43,30 +66,47 @@ public class AmbaruCharacter : MonoBehaviour
         float bdelta = 0.0f;
         
 
-        RaycastHit hit;
-        Ray downRay = new Ray(FrontWheel.transform.position + Vector3.up* collisionOffset, -Vector3.up);
 
+        RaycastHit hit;
+        Ray downRay = new Ray(transform.position + fwheelpos + Vector3.up * collisionOffset, -Vector3.up);
         if (Physics.Raycast(downRay, out hit, 1000.0f, mask))
         {
-            if(grav * Time.deltaTime> hit.distance - collisionOffset)
-            {
-                grav = 0.0f;
-                bdelta = -hit.distance + collisionOffset;
-            }
-            else
-            {
-                grav += gravity*Time.deltaTime;
-                bdelta = (-grav) * Time.deltaTime;
-            }
-            
+            FrontWheel.transform.localPosition = new Vector3(fwheelpos.x, -hit.distance + collisionOffset, fwheelpos.z);
+            //fWheelXm = +hit.distance - collisionOffset;
         }
 
-        
+        if (Mathf.Round(hit.distance*100.0f) / 100.0f != Mathf.Round(oldhitdist* 100.0f) / 100.0f)
+        {
+            Debug.Log("Should Reset, hit: "+hit.distance+" oldhit: "+oldhitdist);
+            fWheelXm = FrontWheelWell.transform.localPosition.y - hit.distance; //gotta fix this later, idk what the deal is.  closest is "hit.distance - oldhitdist" but it should update from where the wheel well actually is... 
+            ResetOsc();
+        }
+        oldhitdist = hit.distance;
+            /*
+            RaycastHit hit;
+            Ray downRay = new Ray(FrontWheel.transform.position + Vector3.up* collisionOffset, -Vector3.up);
 
-        transform.position = transform.position + (Vector3.up * bdelta);
+            if (Physics.Raycast(downRay, out hit, 1000.0f, mask))
+            {
+                if(grav * Time.deltaTime> hit.distance - collisionOffset)
+                {
+                    grav = 0.0f;
+                    bdelta = -hit.distance + collisionOffset;
+                }
+                else
+                {
+                    grav += gravity*Time.deltaTime;
+                    bdelta = (-grav) * Time.deltaTime;
+                }
 
-      
+            }
 
+            */
+
+            //transform.position = transform.position + (Vector3.up * bdelta);
+
+
+        OscillateBody(fWheelXm);
 
         //transform.tran
         //transform.Translate(moveVertical * transform.forward * Time.deltaTime * speed);
@@ -74,7 +114,17 @@ public class AmbaruCharacter : MonoBehaviour
 
     }
     
-    
+    void OscillateBody(float xm)
+    {
+        FrontWheelWell.transform.localPosition = Vector3.up * xm * Mathf.Exp((-damping * (Time.deltaTime+oldtimeval1)) / (2 * wheelMass)) * Mathf.Cos(omegaprime * Time.deltaTime + oldtimeval2) + FrontWheel.transform.localPosition;
+        oldtimeval1 += Time.deltaTime;
+        oldtimeval2 = omegaprime * Time.deltaTime + oldtimeval2;
+    }
 
+    void ResetOsc()
+    {
+        oldtimeval1 = 0.0f;
+        oldtimeval2 = 0.0f;
+    }
     
 }
